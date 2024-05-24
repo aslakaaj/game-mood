92c461f2d64b02caf7977c599cb6c62b9e51a912
Aslak Aarflot Jønsson <aslakaaj@gmail.com>
2024-05-24 10:19:32 +0200

    Users may now click through the games reccommended

--- app/games/[slug]/page.js
+++ app/games/[slug]/page.js
@@ -1,7 +1,8 @@
-import mongodb from "mongodb";
+"use client";
 import { notFound } from "next/navigation";
+import { useState } from "react";
 
-async function getData() {
+async function fetchData() {
   const res = await fetch("http://localhost:3000/api/games", {
     cache: "no-store",
   });
@@ -12,11 +13,14 @@ async function getData() {
   return res.json();
 }
 
-export default async function RecomenderPage({ params }) {
+const filteredArray = [];
+const indexes = [];
+
+function RecomenderPage({ params, data }) {
   console.log(params.slug);
 
-  const data = await getData();
-  const filteredArray = [];
+  const [index, setIndex] = useState(0);
+  let rndNum;
 
   for (let i = 0; i < data.length; i++) {
     if (data[i].Feeling.toLowerCase() === params.slug) {
@@ -24,41 +28,55 @@ export default async function RecomenderPage({ params }) {
     }
   }
 
-  let rndNum = Math.floor(Math.random() * filteredArray.length);
+  rndNum = Math.floor(Math.random() * filteredArray.length);
+  indexes.push(rndNum);
+
+  function nextClickHandler() {
+    if (index === indexes.length - 1) {
+      rndNum = Math.floor(Math.random() * filteredArray.length);
+      indexes.push(rndNum);
+    }
+    setIndex((prevIndex) => prevIndex + 1);
+    console.log(indexes);
+  }
+
+  function prevClickHandler() {
+    setIndex((prevIndex) => prevIndex - 1);
+    console.log(indexes);
+  }
 
   return (
-    <div>
-      {
-        <div className="card w-1/2 my-10 bg-base-100 shadow-xl relative left-1/2 -translate-x-1/2">
-          <figure>
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-            <div className="flex justify-between">
-            <h2 className="card-title">{filteredArray[rndNum].Title}</h2>
-            <h2 className="bg-green-600 w-7 h-7 text-center text-lg font-bold m-0 rounded">{filteredArray[rndNum].Metacritic}</h2>
-            </div>
-            <p>{filteredArray[rndNum].Description}</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Next!</button>
-            </div>
-          </div>
+    <div className="card w-1/2 my-10 bg-base-100 shadow-xl relative left-1/2 -translate-x-1/2">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <div className="flex justify-between">
+          <h2 className="card-title">{filteredArray[indexes[index]].Title}</h2>
+          <h2 className="bg-green-600 w-7 h-7 text-center text-lg font-bold m-0 rounded">
+            {filteredArray[indexes[index]].Metacritic}
+          </h2>
         </div>
-        /* {
-        for (let i = 0; i < data.length; i++) {
-            if (data[i].Feeling) {
-                
-            }
-            
-        }
-      
-      : (
-        <div>No data available</div>
-      )} */
-      }
+        <p>{filteredArray[indexes[index]].Description}</p>
+        <div className="card-actions justify-end">
+          {index >> 0 ? (
+            <button className="btn btn-secondary" onClick={prevClickHandler}>
+              Previous
+            </button>
+          ) : null}
+          <button className="btn btn-secondary" onClick={nextClickHandler}>
+            Next!
+          </button>
+        </div>
+      </div>
     </div>
   );
+}
+
+export default async function Page({ params }) {
+  const data = await fetchData();
+  return <RecomenderPage params={params} data={data} />;
 }
--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home () {
 return(
-    <div className="grid md:grid-cols-4 gap-3 w-1/2 relative left-1/2 -translate-x-1/2">
+    <div className="grid mt-9 grid-cols-2 md:grid-cols-4 gap-3 w-1/2 relative left-1/2 -translate-x-1/2">
         <Link href={"/games/happiness"} className="btn btn-active btn-secondary text-white">😁 Happy</Link>
         <Link href={"/games/sadness"} className="btn btn-active btn-secondary text-white">😔 Sad</Link>
         <Link href={"/games/fear"} className="btn btn-active btn-secondary text-white">😨 Fear</Link>
@@ -11,7 +11,7 @@ return(
         <Link href={"/games/disgust"} className="btn btn-active btn-secondary text-white">🤢 Disgusted</Link>
         <Link href={"/games/love"} className="btn btn-active btn-secondary text-white">🥰 In Love</Link>
         <Link href={"/games/trust"} className="btn btn-active btn-secondary text-white">🤓 Trust</Link>
-        <Link href={"/games/anticipation"} className="btn btn-active btn-secondary text-white">🤯 Anticipated</Link>
+        <Link href={"/games/anticipation"} className="btn btn-active btn-secondary text-white md:col-start-2">🤯 Anticipated</Link>
         <Link href={"/games/guilt"} className="btn btn-active btn-secondary text-white">🫤 Guilt</Link>
     </div>
 );
